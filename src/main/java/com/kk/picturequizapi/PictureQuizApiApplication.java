c44e9d8d6f4a06c4a1881758d875e975c8e2e70b
kyoohyun <kgh2120@naver.com>
2022-08-26 17:12:30 +0900

    Initial commit

---
+++ src/main/java/com/kk/picturequizapi/PictureQuizApiApplication.java
@@ -0,0 +1,13 @@
+package com.kk.picturequizapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PictureQuizApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PictureQuizApiApplication.class, args);
+    }
+
+}
