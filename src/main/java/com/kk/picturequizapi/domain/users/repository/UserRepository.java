a5a9148f03de9810e50bf4d8769d11ec33bd20c5
kyoohyun <kgh2120@naver.com>
2022-08-29 14:15:04 +0900

    fix : 빌드 성공하도록 변경 #3
    - UserRepository extend 부분 삭제

--- src/main/java/com/kk/picturequizapi/domain/users/repository/UserRepository.java
+++ src/main/java/com/kk/picturequizapi/domain/users/repository/UserRepository.java
@@ -1,6 +1,4 @@
 package com.kk.picturequizapi.domain.users.repository;
 
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository {
+public interface UserRepository{
 }
