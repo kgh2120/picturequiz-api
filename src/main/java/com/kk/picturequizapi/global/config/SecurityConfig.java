9c526d3d84051277e67f0ef2b168c93bd6c34319
kyoohyun <kgh2120@naver.com>
2022-08-29 15:04:45 +0900

    ADD : Spring security 설정 클래스 생성 #5

---
+++ src/main/java/com/kk/picturequizapi/global/config/SecurityConfig.java
@@ -0,0 +1,19 @@
+package com.kk.picturequizapi.global.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+        return httpSecurity
+                .csrf().disable()
+                .cors().and().build();
+    }
+}
