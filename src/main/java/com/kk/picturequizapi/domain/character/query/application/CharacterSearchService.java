ddcfc882330cdba11bd1141797492af7082a292a
kyoohyun <kgh2120@naver.com>
2022-10-11 14:20:43 +0900

    FEAT : Redis 캐시 구현 #29

--- src/main/java/com/kk/picturequizapi/domain/character/query/application/CharacterSearchService.java
+++ src/main/java/com/kk/picturequizapi/domain/character/query/application/CharacterSearchService.java
@@ -3,6 +3,7 @@ package com.kk.picturequizapi.domain.character.query.application;
 import com.kk.picturequizapi.domain.character.query.dao.CharacterSearchDao;
 import com.kk.picturequizapi.domain.character.query.dto.CharacterSearch;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -17,6 +18,7 @@ public class CharacterSearchService {
 
 
     // 이름으로 캐릭터를 검색하여 상위 5개의 검색 결과를 불러온다.
+    @Cacheable(key = "#name", cacheNames = "characters")
     public List<CharacterSearch> find5CharactersByName(String name) {
         return characterSearchDao.findTop5ByNameStartsWithOrderByName(name);
     }
--- src/main/java/com/kk/picturequizapi/domain/character/query/dto/CharacterSearch.java
+++ src/main/java/com/kk/picturequizapi/domain/character/query/dto/CharacterSearch.java
@@ -7,10 +7,11 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 import javax.persistence.Table;
+import java.io.Serializable;
 
 @Entity
 @Table(name = "character") @Getter @NoArgsConstructor
-public class CharacterSearch {
+public class CharacterSearch implements Serializable {
 
     @Id @Column(name = "character_id")
     private Long characterId;
