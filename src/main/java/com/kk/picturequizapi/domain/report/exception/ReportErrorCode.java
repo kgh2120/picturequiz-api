8af9634acb6ca3404104991039ce554835e34d16
kyoohyun <kgh2120@naver.com>
2023-01-18 22:47:45 +0900

    CHORE : 중복 예외 클래스 추가

--- src/main/java/com/kk/picturequizapi/domain/report/exception/ReportErrorCode.java
+++ src/main/java/com/kk/picturequizapi/domain/report/exception/ReportErrorCode.java
@@ -11,6 +11,8 @@ public enum ReportErrorCode implements ErrorCode {
     INPUT_NULL_DATA_ON_QUIZ_ID(HttpStatus.INTERNAL_SERVER_ERROR, "r-0002","신고 데이터가 생성될 때, 퀴즈 ID에 null 이 들어갔습니다."),
     INPUT_NULL_DATA_ON_REPORTER(HttpStatus.INTERNAL_SERVER_ERROR, "r-0003","신고 데이터가 생성될 때, 신고자에 null 이 들어갔습니다."),
     INPUT_NULL_DATA_ON_REPORT_ID(HttpStatus.INTERNAL_SERVER_ERROR, "r-0004","신고 데이터가 생성될 때, 신고ID에 null 이 들어갔습니다."),
+
+    ALREADY_REPORTED_QUIZ(HttpStatus.BAD_REQUEST, "r-0005", "해당 사항으로 이미 신고가 접수되었습니다."),
     
     
 
