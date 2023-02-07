6b770bb851e1683462ad7f59ba02af7b214bfe36
kyoohyun <kgh2120@naver.com>
2023-02-08 00:56:02 +0900

    FIX : 관리자 조회 & 삭제 부분 수정

--- src/main/java/com/kk/picturequizapi/domain/admin/command/ui/AdminAccountDeleteController.java
+++ src/main/java/com/kk/picturequizapi/domain/admin/command/ui/AdminAccountDeleteController.java
@@ -5,6 +5,7 @@ import com.kk.picturequizapi.domain.users.dto.UserIdRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,10 +15,10 @@ public class AdminAccountDeleteController {
 
     private final AdminAccountDeleteService adminAccountDeleteService;
 
-    @DeleteMapping("/admin")
-    public ResponseEntity<Void> deleteAdminAccount(@RequestBody UserIdRequestDto dto){
+    @DeleteMapping("/admin/{adminId}")
+    public ResponseEntity<Void> deleteAdminAccount(@PathVariable("adminId") long adminId){
 
-        adminAccountDeleteService.deleteAdminAccount(dto.getUserId());
+        adminAccountDeleteService.deleteAdminAccount(adminId);
         return ResponseEntity.ok().build();
     }
 
